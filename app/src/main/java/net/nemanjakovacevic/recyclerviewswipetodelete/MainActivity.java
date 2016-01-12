30a97c95bb0fc95c732c2b7b488f9f7bd3061c80
Nemanja Kovacevic <nemanja.kovacevic@gmail.com>
2016-01-11 18:43:17 -0800

    initial commit

---
+++ app/src/main/java/net/nemanjakovacevic/recyclerviewswipetodelete/MainActivity.java
@@ -0,0 +1,343 @@
+package net.nemanjakovacevic.recyclerviewswipetodelete;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        setUpRecyclerView();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_item_undo_checkbox) {
+            item.setChecked(!item.isChecked());
+            ((TestAdapter)mRecyclerView.getAdapter()).setUndoOn(item.isChecked());
+        }
+        if (item.getItemId() == R.id.menu_item_add_5_items) {
+            ((TestAdapter)mRecyclerView.getAdapter()).addItems(5);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setUpRecyclerView() {
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(new TestAdapter());
+        setUpItemTouchHelper();
+        setUpAnimationDecoratorHelper();
+    }
+
+    private void setUpAnimationDecoratorHelper() {
+        mRecyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+
+            Drawable background;
+            boolean initiated;
+
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+
+                if (!initiated) {
+                    init();
+                }
+
+                if (parent.getItemAnimator().isRunning()) {
+                    // find first child with translationY > 0
+                    // and last one with translationY < 0
+                    // we're after a rect that is not covered in recycler-view views at this point in time
+
+                    View lastViewComingDown = null;
+                    View firstViewComingUp = null;
+
+                    // fixed
+                    int left = 0;
+                    int right = parent.getWidth();
+
+                    // we need to find out
+                    int top = 0;
+                    int bottom = 0;
+
+                    // find relevant translating views
+                    int childCount = parent.getLayoutManager().getChildCount();
+                    for (int i = 0; i < childCount; i++) {
+                        View child = parent.getLayoutManager().getChildAt(i);
+                        if (child.getTranslationY() < 0) {
+                            // view is coming down
+                            lastViewComingDown = child;
+                        } else if (child.getTranslationY() > 0) {
+                            // view is coming up
+                            if (firstViewComingUp == null) {
+                                firstViewComingUp = child;
+                            }
+                        }
+                    }
+
+                    if (lastViewComingDown != null && firstViewComingUp != null) {
+                        // views are coming down AND going up to fill the void
+                        top = lastViewComingDown.getBottom() + (int)lastViewComingDown.getTranslationY();
+                        bottom = firstViewComingUp.getTop() + (int) firstViewComingUp.getTranslationY();
+                    } else if (lastViewComingDown != null) {
+                        // views are going down to fill the void
+                        top = lastViewComingDown.getBottom() + (int) lastViewComingDown.getTranslationY();
+                        bottom = lastViewComingDown.getBottom();
+                    } else  if (firstViewComingUp != null){
+                        // views are coming up to fill the void
+                        top = firstViewComingUp.getTop();
+                        bottom = firstViewComingUp.getTop() + (int) firstViewComingUp.getTranslationY();
+                    }
+
+                    Drawable background = new ColorDrawable(Color.RED);
+                    background.setBounds(left, top, right, bottom);
+                    background.draw(c);
+
+                }
+                super.onDraw(c, parent, state);
+            }
+
+            private void init() {
+                background = new ColorDrawable(Color.RED);
+                initiated = true;
+            }
+        });
+    }
+
+    private void setUpItemTouchHelper() {
+        // Support library way of implementing swipes on recycler view items
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            Drawable background;
+            Drawable xMark;
+            int xMarkMargin;
+            boolean initiated;
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                int swipedPosition = viewHolder.getAdapterPosition();
+                final TestAdapter adapter = (TestAdapter)mRecyclerView.getAdapter();
+                final String item = adapter.itemAt(swipedPosition);
+//                boolean undoOn = adapter.isUndoOn();
+//                if (undoOn) {
+//                    adapter.pendingRemoval(item);
+//                    adapter.notifyDataSetChanged();
+//                    new Handler().postDelayed(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            adapter.timedRemoval(item);
+//                        }
+//                    }, 3000);
+//                } else {
+//                    adapter.remove(item);
+//                }
+                adapter.remove(item);
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                View itemView = viewHolder.itemView;
+
+                if (viewHolder.getAdapterPosition() == -1) {
+                    return;
+                }
+
+                if (!initiated) {
+                    init();
+                }
+
+                // draw red background
+                background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                background.draw(c);
+
+                // draw x mark
+                int itemHeight = itemView.getBottom() - itemView.getTop();
+                int intrinsicWidth = xMark.getIntrinsicWidth();
+                int intrinsicHeight = xMark.getIntrinsicWidth();
+
+                int xMarkLeft = itemView.getRight() - xMarkMargin - intrinsicWidth;
+                int xMarkRight = itemView.getRight() - xMarkMargin;
+                int xMarkTop = itemView.getTop() + (itemHeight - intrinsicHeight)/2;
+                int xMarkBottom = xMarkTop + intrinsicHeight;
+                xMark.setBounds(xMarkLeft, xMarkTop, xMarkRight, xMarkBottom);
+
+                xMark.draw(c);
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+
+            private void init() {
+                background = new ColorDrawable(Color.RED);
+                xMark = ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_clear_24dp);
+                xMark.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                xMarkMargin = (int) MainActivity.this.getResources().getDimension(R.dimen.ic_clear_margin);
+                initiated = true;
+            }
+
+            @Override0
+            public void onChildDrawOver(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                super.onChildDrawOver(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+        ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    static class TestAdapter extends RecyclerView.Adapter {
+
+        List<String> items;
+        List<String> itemsPendingRemoval;
+        int lastInsertedIndex;
+        boolean undoOn;
+
+        public TestAdapter() {
+            items = new ArrayList<>();
+            itemsPendingRemoval = new ArrayList<>();
+            lastInsertedIndex = 15;
+            // this should give us a couple of screens worth
+            for (int i=1; i<= lastInsertedIndex; i++) {
+                items.add("Item " + i);
+            }
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new TestViewHolder(parent);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            TestViewHolder viewHolder = (TestViewHolder)holder;
+            final String item = items.get(position);
+            if (itemsPendingRemoval.contains(item)) {
+                viewHolder.itemView.setBackgroundColor(Color.RED);
+                viewHolder.titleTextView.setVisibility(View.GONE);
+                viewHolder.undoButton.setVisibility(View.VISIBLE);
+                viewHolder.undoButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        itemsPendingRemoval.remove(item);
+                        notifyItemChanged(items.indexOf(item));
+                    }
+                });
+            } else {
+                viewHolder.itemView.setBackgroundColor(Color.WHITE);
+                viewHolder.titleTextView.setVisibility(View.VISIBLE);
+                viewHolder.titleTextView.setText(item);
+                viewHolder.undoButton.setVisibility(View.GONE);
+                viewHolder.undoButton.setOnClickListener(null);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+
+        public void addItems(int howMany){
+            if (howMany > 0) {
+                for (int i = lastInsertedIndex + 1; i <= lastInsertedIndex + howMany; i++) {
+                    items.add("Item " + i);
+                    notifyItemInserted(items.size() - 1);
+                }
+                lastInsertedIndex = lastInsertedIndex + howMany;
+            }
+        }
+
+        public void setUndoOn(boolean undoOn) {
+            this.undoOn = undoOn;
+        }
+
+        public boolean isUndoOn() {
+            return undoOn;
+        }
+
+        public String itemAt(int position) {
+            if (position < 0 || position >= items.size()) {
+                return null;
+            }
+            return items.get(position);
+        }
+
+        public void pendingRemoval(String item) {
+            if (!itemsPendingRemoval.contains(item)) {
+                itemsPendingRemoval.add(item);
+                int position = items.indexOf(item);
+                notifyItemChanged(position);
+            }
+        }
+
+        public void remove(String item) {
+            if (itemsPendingRemoval.contains(item)) {
+                itemsPendingRemoval.remove(item);
+            }
+            if (items.contains(item)) {
+                int position = items.indexOf(item);
+                items.remove(position);
+                notifyItemRemoved(position);
+            }
+        }
+
+        public void timedRemoval(String item) {
+            if (!itemsPendingRemoval.contains(item)) {
+                // removal no longer pending
+                return;
+            }
+            remove(item);
+        }
+    }
+
+    static class TestViewHolder extends RecyclerView.ViewHolder {
+
+        TextView titleTextView;
+        Button undoButton;
+
+        public TestViewHolder(ViewGroup parent) {
+            super(LayoutInflater.from(parent.getContext()).inflate(R.layout.row_view, parent, false));
+            titleTextView = (TextView) itemView.findViewById(R.id.title_text_view);
+            undoButton = (Button) itemView.findViewById(R.id.undo_button);
+        }
+
+    }
+
+
+
+}
