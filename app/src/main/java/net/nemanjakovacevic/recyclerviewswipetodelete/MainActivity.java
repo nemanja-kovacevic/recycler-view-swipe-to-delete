931d90c623b4794b8ad462d0b9881ee9d626118f
James <jjvg@me.com>
2017-05-08 03:36:50 -0500

    Don't render the icon when swipe event aborted.

--- app/src/main/java/net/nemanjakovacevic/recyclerviewswipetodelete/MainActivity.java
+++ app/src/main/java/net/nemanjakovacevic/recyclerviewswipetodelete/MainActivity.java
@@ -131,6 +131,12 @@ public class MainActivity extends AppCompatActivity {
                     // not interested in those
                     return;
                 }
+                
+                // Don't render the icon when a user aborts a swipe.
+                if (!isCurrentlyActive)
+                {
+                    return;
+                }
 
                 if (!initiated) {
                     init();
